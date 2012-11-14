cdf2f322a0c4c25bb2c17575aa72f710f05864b8
a.bailey <a.bailey@dedam-n494.corp.grimme.com>
2012-11-14 15:41:03 +0100

    Made the bean @Startup

--- src/main/java/org/andy/pf/tfsim/simulator/AsynchDirectionChangeSimulator.java
+++ src/main/java/org/andy/pf/tfsim/simulator/AsynchDirectionChangeSimulator.java
@@ -1,11 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.andy.pf.tfsim.simulator;
 
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
+import javax.ejb.Startup;
 import org.primefaces.push.PushContext;
 import org.primefaces.push.PushContextFactory;
 
@@ -14,6 +11,7 @@ import org.primefaces.push.PushContextFactory;
  * @author a.bailey
  */
 @Singleton
+@Startup
 public class AsynchDirectionChangeSimulator {
 
     final static String[] DIRS = {
@@ -29,13 +27,11 @@ public class AsynchDirectionChangeSimulator {
     
     
     @Schedule(minute = "*", second = "*/1", hour = "*")
-    public void myTimer() {
+    public void pushDirection() {
         String nextDirection = DIRS[(int)(Math.random()*DIRS.length)];
         PushContext ctx = PushContextFactory.getDefault().getPushContext();
         if( ctx != null ) {
             ctx.push("/tfSim", nextDirection);
         }
     }
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
 }
